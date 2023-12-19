b627451588d62c6bd3814857cc34b36dcd94c46d
arv1f <114027957+arv1f@users.noreply.github.com>
2023-12-19 19:23:22 +0300

    Update enoti.ts

--- 14.12.23/enoti.ts
+++ 14.12.23/enoti.ts
@@ -1,11 +1,11 @@
-interface Enot {
+interface Enot {//    Создаем тип JSON описывающий нашего будущего енота
     name: string;
     age:number;
     weight: number;
-    image?: string;
     prompt?: string;
+    image?: string;
 };
-interface Data {
+interface Data {//      Создаем тип JSON элемента, который по API будем отсылать на сервер
     model: string;
     sampler: string;
     prompt: string;
@@ -17,79 +17,51 @@ interface Data {
     loras: {[key: string]: number;};
     image?: string;
 };
-
-function apigenerate(data:Data,enot:Enot) {
-        
-
-        fetch("https://visioncraftapi--vladalek05.repl.co/generate",{
-            method:"POST",
-            headers: {"Content-Type":"application/json"},
-            body:JSON.stringify(data)
-        }).then(data=>{return data.json();}).then(
-            body=>{
-                console.log(body);
-                const image_url: string = body.images[0];
-                const imgElement = document.createElement('img');
-                imgElement.src = image_url
-                let imageElement = <HTMLDivElement>document.getElementById('image');
-                imageElement.appendChild(imgElement);
-                enotCache.saveData({ name: enot.name, age: enot.age, weight: enot.weight , image: image_url});// Сохраняем данные о еноте в кэш
-            });
-        
-    };
-
-class EnotCache {
-    private storageKey: string;
-
-    constructor(storageKey: string) {
-        this.storageKey = storageKey;
-    };
-    // Сохранение данных в localStorage
-    saveData(data: Enot): void {
-        localStorage.setItem(this.storageKey, JSON.stringify(data));
-    };
-    // Получение данных из localStorage
-    getData(): Enot | null {
-        const data = localStorage.getItem(this.storageKey);
-        return data ? JSON.parse(data) : null;
-    };
+let storageKey : string;
+function saveData(data: Enot): void {               // Функция для сохранения данных в кеш
+    localStorage.setItem(storageKey, JSON.stringify(data));
 };
 
-// Использование
-const enotCache = new EnotCache('enotData');
-// Получение данных
+function getData(): Enot | null {               // Функция для взятия данных из кеша
+    const data = localStorage.getItem(storageKey);
+    return data ? JSON.parse(data) : null;
+};
+
+
 let divElement = <HTMLDivElement>document.getElementById('textdiv');
-let enots=enotCache.getData();
-if (enots){
+let enots=getData();
+if (enots){                                 //          ЗАсовываем данные из кеша в сайт
+    console.log(enots);
 let TextEnot: string = 'Name: '+enots.name+'. Age: '+enots.age+'. Weight: '+enots.weight+'.';
 divElement.innerText = TextEnot;// Выводит после перезагрузки страницы
 const imgElement = document.createElement('img');
-imgElement.src = enots.image
+imgElement.src = String(enots.image);
 let imageElement = <HTMLDivElement>document.getElementById('image');
 imageElement.appendChild(imgElement);
 }
 
-function displayInputText(): void {
-    let divElement = <HTMLDivElement>document.getElementById('textdiv');
-    
+function main() {
 
     let inputElementName = <HTMLInputElement>document.getElementById('name');
-    let inputElementAge = <HTMLInputElement>document.getElementById('age');
+    let inputElementAge = <HTMLInputElement>document.getElementById('age');             //Подключаем элементы сайта
     let inputElementWeight = <HTMLInputElement>document.getElementById('weight');
 
-    const enot: Enot = {
-        name: inputElementName.value,
-        weight: Number(inputElementAge.value),
-        age: Number(inputElementWeight.value),
-    };
-    
+
     let apiKey='1fcdf54c-559b-40e0-a8ff-2f101e3cc449';
     const loras = { "3DMM_V12": 1, "GrayClay_V1.5.5": 2, "eye_size_slider_v1": 3, "age_slider_v20": 1,};
     const prompt = "Draw a beautiful cute animal raccoon by "+
-    "<br> name "+enot.name+
-    ", weight "+enot.weight+
-    " kilogram, <br> and so old "+enot.name+".;"
-    const DataEnota:Data={
+        "<br> name "+<HTMLInputElement>document.getElementById('name')+
+        ", weight "+<HTMLInputElement>document.getElementById('weight')+
+        " kilogram, <br> and so old "+<HTMLInputElement>document.getElementById('age')+".;"
+
+    const enot: Enot = {
+            name: inputElementName.value,
+            weight: Number(inputElementAge.value),
+            age: Number(inputElementWeight.value),
+            prompt: prompt,
+        };
+
+    const DataEnota:Data={                  // API: Данные которые отправятся на сервер
         model: "absolutereality_v1.8.1",
         sampler: "LMS",
         prompt:prompt,
@@ -99,24 +71,23 @@ function displayInputText(): void {
         cfg_scale: 5,
         steps: 20,
         loras: loras,
-    }
-    apigenerate(DataEnota,enot);
-    
-
-    
-
-    let TextEnot: string = 'Name: '+enot.name+'. Age: '+enot.age+'. Weight: '+enot.weight+'.';
-    //let TextEnot: string = 'Name: '+inputElementName.value+'. Age: '+inputElementAge.value+'. Weight: '+inputElementWeight.value+'.';
-    divElement.innerText = TextEnot;
-
-    
-
-
+    };
+    fetch("https://visioncraftapi--vladalek05.repl.co/generate",{           // API: Отправляем данные на сервер и возвращаем картинку
+            method:"POST",
+            headers: {"Content-Type":"application/json"},
+            body:JSON.stringify(DataEnota)
+        }).then(data=>{return data.json();}).then(
+            body=>{
+                const image_url: string = body.images[0];
+                const imgElement = document.createElement('img');
+                imgElement.src = image_url
+                let imageElement = <HTMLDivElement>document.getElementById('image');
+                imageElement.appendChild(imgElement);
+                saveData({ name: enot.name, weight: enot.weight , age: enot.age, image: image_url});// Сохраняем данные о еноте в кэш
+                enot.image=image_url;
+            });
 }
 
 let buttonElement = <HTMLButtonElement>document.getElementById('mainButton');
-buttonElement.addEventListener('click', displayInputText);
-
-
-
-//npx tsc typescript-intro/src/enoti/enoti.ts
+buttonElement.addEventListener('click', main);// Запускаем все
+//npx tsc typescript-intro/src/enoti/enoti2.ts
